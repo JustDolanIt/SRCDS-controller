741823a07989598f0c66e637e328e88890db0193
JustDolanIt <4epaeb@gmail.com>
2014-04-04 12:51:02 -0400

    Added work with Workshop (requires Collection number in 'workshop' file and 'authkey' file with API key) and on start check if one already running

--- serverController.sh
+++ serverController.sh
@@ -1,30 +1,59 @@
 #!/bin/bash
 
-# ARGS = { $1=command, $2=map/command, $3=mode }
+# ARGS = { $1=command, $2=map/mode, $3=mode }
 
 SRCDS_PATH=/home/dedicated/steamcmd/gmod/srcds_run
 SESSION_NAME=GModServer
 GAME=garrysmod
 
+DEFAULTMAP=gm_construct
+DEFAULTGAMEMODE=sandbox
+
 case "$1" in
   start ) 
-          screen -S $SESSION_NAME -d -m $SRCDS_PATH -game $GAME +map $2 +gamemode $3 # Screen creates session, sends command there and detatches. When command finished, session will finish too.
-          echo $2 $3 | cat > status
-          echo "Server started";;
+          if [ `ps u | grep srcds | grep -v grep | awk -F ' ' '{print $2}' | head -n1` ]
+          then
+            echo 'Server is already started'
+          else
+            if [[ -z "$2" ]]
+            then
+              screen -S $SESSION_NAME -d -m $SRCDS_PATH -game $GAME +host_workshop_collection `cat workshop` -authkey `cat authkey` +map $DEFAULTMAP +gamemode $DEFAULTGAMEMODE # Screen creates session, sends command there and detatches. When command finished, session will finish too.
+            else
+              screen -S $SESSION_NAME -d -m $SRCDS_PATH -game $GAME +host_workshop_collection `cat workshop` -authkey `cat authkey` +map $2 +gamemode $3
+            fi
+            echo $2 $3 | cat > status
+            echo "Server started"
+          fi;;
   stop )  
-          screen -S $SESSION_NAME -p 0 -X stuff "quit$(printf \\r)" # Screen sends command. 'stuff' with X - choosing buffer for command you send. printf sends Enter to the end of string
+          screen -S $SESSION_NAME -p 0 -X stuff "quit $(printf \\r)" # Screen sends command. 'stuff' with X - choosing buffer for command you send. printf sends Enter to the end of string (PS works time to time)
           rm status
           echo "Server stopped" ;;
+  changemap )
+          if [ `ps u | grep srcds | grep -v grep | awk -F ' ' '{print $2}' | head -n1` ];
+          then
+            screen -S $SESSION_NAME -p 0 -X stuff "map $2 $(printf \\r)"
+            echo $2 `awk -F ' ' '{print $2}' status`  | cat >status 
+          else
+            echo "Server is Halted"
+          fi;;
+  changemode)
+          if [ `ps u | grep srcds | grep -v grep | awk -F ' ' '{print $2}' | head -n1` ];
+          then
+            screen -S $SESSION_NAME -p 0 -X stuff "gamemode $2 $(printf \\r)"
+            echo `awk -F ' ' '{print $1}' status` $2  | cat >status 
+          else
+            echo "Server is Halted"
+          fi;;
   kill )  
-          for PIDEL in `ps u | grep srcds | awk -F ' ' '{print $2}'| head -n 2` # screen session will also stop
+          for PIDAL in `ps u | grep srcds | grep -v grep | awk -F ' ' '{print $2}'| head -n 2` # screen session will also stop
           do 
-            kill $PIDEL
+            kill $PIDAL
           done
           rm status
           echo "All processes killed"
           ;;
   status )
-          if [ `ps u | grep srcds | wc -l | awk -F ' ' '{print $1}'` -eq 1 ];
+          if [ `ps u | grep srcds | grep -v grep | wc -l | awk -F ' ' '{print $1}'` -eq 0 ];
           then
             echo "Halted"
           else
@@ -32,11 +61,11 @@ case "$1" in
           fi;;
   pids )  
           echo "Server pids:"
-          for PIDEL in `ps u | grep srcds | awk -F ' ' '{print $2}'| head -n 2` # screen session will also stop
+          for PIDAL in `ps u | grep srcds | grep -v grep | awk -F ' ' '{print $2}'` # screen session will also stop
           do 
-            echo $PIDEL
+            echo $PIDAL
           done;;
   * ) 
-          echo; echo "Usage:"; echo; echo "  start <MAP> <MODE> - start server with MAP and MODE."; echo "  stop - stops server."; echo "  kill - kills server processes."; echo "  status - get status of server."; echo "  pids - get pids of server and screen"; echo;;
+          echo; echo "Usage:"; echo; echo "  start <MAP> <MODE> - start server with MAP and MODE."; echo "  stop - stops server."; echo "  changemap <MAP> - changes current map"; echo "  changemode <MODE> - changes current gamemode"; echo "  kill - kills server processes."; echo "  status - get status of server."; echo "  pids - get pids of server and screen"; echo;;
 esac
 
